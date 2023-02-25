0a34e1a9db792be8a2417a339d3f929de00f1f02
aa2669 <48959400+aa2669@users.noreply.github.com>
2023-02-25 00:09:13 -0500

    Create config.php

---
+++ Backend/config.php
@@ -0,0 +1,16 @@
+<?php
+// Database configuration
+define('DB_USERNAME', 'Linux');
+define('DB_PASSWORD', 'Linux');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'userData');
+
+// RabbitMQ configuration
+define('RMQ_HOST', 'localhost');
+define('RMQ_PORT', 5672);
+define('RMQ_USERNAME', 'test');
+define('RMQ_PASSWORD', 'test');
+define('RMQ_VIRTUAL_HOST', 'it490');
+define('RMQ_EXCHANGE_NAME', 'it490_exchange');
+define('RMQ_QUEUE_NAME', 'it490_queue');
+?>
